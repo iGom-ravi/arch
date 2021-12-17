0bb4e12a89cd0ae0ee8e8ab31cb241f6bd1ebd8a
iGom-ravi <95466376+iGom-ravi@users.noreply.github.com>
2021-12-17 15:02:11 +0530

    Create Sharedlibrary.groovy

---
+++ shared/src/com/sharedlib/Sharedlibrary.groovy
@@ -0,0 +1,12 @@
+package com.shared;
+
+public class shared{
+   def steps
+  public Shared(steps){
+    this.steps = steps
+  }
+  
+  public void startBuild(){
+    steps.echo "Hello, world"
+  }
+}
